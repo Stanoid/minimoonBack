09253605a906c23d4996b1c8354abab62c837184
Stanoid <mujahidwadmohammad@gmail.com>
2024-04-28 00:30:18 +0300

    edit product

--- src/api/product/controllers/product.js
+++ src/api/product/controllers/product.js
@@ -31,8 +31,6 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
       } catch (err) {
         return "unauthorized request catch triggred";
       }
-
-
       var url_parts = url.parse(ctx.request.url, true);
       var query = url_parts.query;
       switch (query.func) {
@@ -70,6 +68,22 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
           return res;
           break;
 
+
+
+          case "getAllProductsAdmin":
+
+          const resp = await strapi.db.query("api::product.product").findMany({
+
+              offset:parseInt(query.page),
+              limit:12,
+              select: ["*"],
+              populate: ["varients","varients.size","varients.color","subcatagory","seller"],
+            });
+
+            const sanitizedEntity = await this.sanitizeOutput(resp, ctx);
+            return sanitizedEntity;
+            break;
+
         default:
           return "Defaulting from Authed, You screwed up badly fam (: ";
           break;
@@ -83,8 +97,8 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
 
         const res = await strapi.db.query("api::product.product").findMany({
 
-            offset:parseInt(query.page),
-            limit:12,
+            // offset:parseInt(query.page),
+            // limit:12,
             select: ["*"],
             populate: ["varients","subcatagory","seller"],
           });
@@ -93,11 +107,39 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
           return sanitizedEntity;
           break;
 
-        default:
 
-          return "Not a valid function name (: ";
+
+          case "getFullProduct":
+
+          const entity = await strapi.service("api::product.product").findOne(id, {
+            select: ["*"],
+            populate: ["varients","varients.color","varients.size","subcatagory","subcatagory.catagory"],
+            });
+            // const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+            // return this.transformResponse(sanitizedEntity);
+            return entity;
 
           break;
+
+          case "getProductVariants":
+
+            const entityb = await strapi.service("api::product.product").findOne(id, {
+                select: ["*"],
+                populate: ["varients","varients.size","varients.color"],
+              });
+              const sanitizedEntityb = await this.sanitizeOutput(entityb, ctx);
+
+              return sanitizedEntityb;
+
+            break;
+
+
+            default:
+
+            return "Not a valid function name (: ";
+
+            break;
       }
     }
   },
@@ -180,8 +222,6 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
 
        if(utype==1){
         const {nameen,namear,descen,descar,subc,color,size,stock,price,imgs} = ctx.request.body;
-
-
         const entry = await strapi.entityService.create('api::varient.varient', {
           data: {
             price:price,
@@ -230,37 +270,105 @@ module.exports = createCoreController('api::product.product', ({ strapi }) => ({
 
          break;
      }
-
-
-
-
-
-      // try {
-
-
-
-      // } catch (err) {
-      //   return "nauthorized request catch triggred";
-      // }
-
-
-
-
-
-
-
   } else {
     return "unauthorized access."
    }
-
-
-
   },
 
 
 
+  async update(ctx) {
+    const { id } = ctx.params;
 
 
+    console.log("aaaaaaaaa",id)
+
+    if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+      const udata = await strapi.plugins[
+        "users-permissions"
+      ].services.jwt.getToken(ctx);
+     udata.id;
+     const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+    udata.id,
+    );
+     const utype = user.type
+     var url = require("url");
+     var url_parts = url.parse(ctx.request.url, true);
+     var query = url_parts.query;
+     switch (query.func) {
+
+       case "EditProduct":
+       if(utype==1){
+
+        const {nameen,namear,descen,descar,subc,varients} = ctx.request.body;
+
+
+        console.dir(varients);
+
+      let varray = []
+        for (const key in varients) {
+          if (Object.hasOwnProperty.call(varients, key)) {
+        if(varients[key].id){
+
+       varray.push(varients[key].id)
+
+        }else{
+
+      const entry = await strapi.entityService.create('api::varient.varient', {
+            data: {
+              price:varients[key].attributes.price,
+              stock:varients[key].attributes.stock,
+              color:varients[key].attributes.color.id,
+              size: varients[key].attributes.size.id,
+              publishedAt :  Date.now() ,
+            },
+          });
+        const verid = entry.id;
+        varray.push(verid);
+
+
+
+        }
+
+          }
+        }
+
+
+
+     // console.log(imgs);
+      const productentry = await strapi.entityService.update('api::product.product',id, {
+
+      data: {
+          status:true,
+          name_ar:namear,
+          name_en:nameen,
+          description_ar:descar,
+          description_en: descen,
+          varients: varray,
+          subcatagory:subc,
+          updatedAt: Date.now()
+        },
+      });
+
+      return productentry
+
+       }else{
+         return "unauthorized (:";
+       }
+        break;
+
+
+
+          default:
+        return "no function selected"
+
+         break;
+     }
+  } else {
+    return "unauthorized access."
+   }
+  },
 
 
  }));
--- src/api/size/controllers/size.js
+++ src/api/size/controllers/size.js
@@ -1,9 +1,76 @@
 'use strict';
-
+// @ts-nocheck
 /**
  * size controller
  */
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::size.size');
+module.exports = createCoreController('api::size.size', ({ strapi }) => ({
+
+  async create(ctx) {
+    const { id } = ctx.params;
+
+    if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
+
+
+
+      const udata = await strapi.plugins[
+        "users-permissions"
+      ].services.jwt.getToken(ctx);
+
+     udata.id;
+
+
+     const user = await strapi.entityService.findOne(
+      "plugin::users-permissions.user",
+    udata.id,
+
+    );
+
+     const utype = user.type
+
+
+     var url = require("url");
+     var url_parts = url.parse(ctx.request.url, true);
+     var query = url_parts.query;
+     switch (query.func) {
+       case "AddSize":
+//console.log("Ssss",utype);
+
+       if(utype==1){
+        const {name_ar,name_en,icon,status} = ctx.request.body;
+
+
+        const entry = await strapi.entityService.create('api::size.size', {
+          data: {
+            name_ar:name_ar,
+            name_en:name_en,
+            icon:icon,
+            status: status,
+            publishedAt :  Date.now() ,
+          },
+        });
+  return entry
+
+       }else{
+         return "unauthorized (:";
+       }
+         break;
+          default:
+        return "no function selected"
+         break;
+     }
+      // try {
+      // } catch (err) {
+      //   return "nauthorized request catch triggred";
+      // }
+  } else {
+    return "unauthorized access."
+   }
+
+
+
+  },
+
+}));
