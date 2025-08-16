f4a30cc9e4c1cb9b978d7998c85bc6e9e577406f
najialii <najialii249@gmail.com>
2025-08-16 19:58:57 +0300

    setfeatimg

--- src/api/subcatagory/controllers/subcatagory.js
+++ src/api/subcatagory/controllers/subcatagory.js
@@ -16,92 +16,68 @@ module.exports = createCoreController('api::subcatagory.subcatagory', ({ strapi 
 
     if (ctx.request && ctx.request.header && ctx.request.header.authorization) {
 
-
-
       const udata = await strapi.plugins[
         "users-permissions"
       ].services.jwt.getToken(ctx);
 
-     udata.id;
+      const user = await strapi.entityService.findOne(
+        "plugin::users-permissions.user",
+        udata.id,
+      );
 
+      const utype = user.type;
 
-     const user = await strapi.entityService.findOne(
-      "plugin::users-permissions.user",
-    udata.id,
+      var url = require("url");
+      var url_parts = url.parse(ctx.request.url, true);
+      var query = url_parts.query;
 
-    );
+      switch (query.func) {
+        case "AddSubCat":
+          if (utype == 1) {
+            const { name_ar, name_en, catagory } = ctx.request.body;
+            const { files } = ctx.request;
 
-     const utype = user.type
+            let imageEntry = null;
 
+            if (files && files.img) {
+              const fileData = Array.isArray(files.img) ? files.img : [files.img];
 
-     var url = require("url");
-     var url_parts = url.parse(ctx.request.url, true);
-     var query = url_parts.query;
-     switch (query.func) {
-      case "AddSubCat":
-        if (utype == 1) {
-          const { name_ar, name_en, catagory } = ctx.request.body;
-          const { files } = ctx.request;
+              const uploaded = await strapi
+                .plugin("upload")
+                .service("upload")
+                .upload({
+                  data: {},
+                  files: fileData,
+                });
 
-          let imageEntry = null;
-
-          if (files && files.img) {
-            // Handle array or single object
-            const fileData = Array.isArray(files.img) ? files.img : [files.img];
-
-            const uploaded = await strapi
-              .plugin("upload")
-              .service("upload")
-              .upload({
-                data: {},
-                files: fileData,
-              });
-
-            if (uploaded.length > 0) {
-              imageEntry = uploaded[0];
+              if (uploaded.length > 0) {
+                imageEntry = uploaded[0];
+              }
             }
+
+            const entry = await strapi.entityService.create(
+              "api::subcatagory.subcatagory",
+              {
+                data: {
+                  name_ar,
+                  name_en,
+                  catagory,
+                  status: true,
+                  publishedAt: new Date(),
+                  ...(imageEntry ? { img: imageEntry.id } : {}),
+                },
+              }
+            );
+
+            return entry;
+          } else {
+            return "unauthorized (:";
           }
-
-          const entry = await strapi.entityService.create(
-            "api::subcatagory.subcatagory",
-            {
-              data: {
-                name_ar,
-                name_en,
-                catagory,
-                status: true,
-                publishedAt: new Date(),
-                ...(imageEntry ? { img: imageEntry.id } : {}),
-              },
-            }
-          );
-
-          return entry;
-        } else {
-          return "unauthorized (:";
-        }
-
-
-
-
-     }
-      // try {
-      // } catch (err) {
-      //   return "nauthorized request catch triggred";
-      // }
-  } else {
-    return "unauthorized access."
-   }
-
-
-
+      }
+    } else {
+      return "unauthorized access."
+    }
   },
-
-
-
-
-
-
 
   async find(ctx) {
     let regid = null;
@@ -115,176 +91,127 @@ module.exports = createCoreController('api::subcatagory.subcatagory', ({ strapi 
 
         regid = udata.id;
       } catch (err) {
-        return "unauthorized request catch triggred";
+        return "unauthorized request catch triggered";
       }
       var url_parts = url.parse(ctx.request.url, true);
       var query = url_parts.query;
       switch (query.func) {
+        case "getRelatedsubcats":
+          const ressub = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+            select: ["*"],
+            where: { catagory: { id: query.cid } },
+            filters: { $not: { id: query.sid } },
+            populate: ["products","products.varients","products.varients.colors","products.subcatagory","products.seller"],
+          });
 
-        //authed subcat acces goes here
+          return await this.sanitizeOutput(ressub, ctx);
 
+        case "getTopSecSubcats":
+          const topSubcats = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+            select: ["*"],
+            where: { topsec: true, status: true },
+            populate: ["img", "catagory", "products"],
+          });
 
+          return await this.sanitizeOutput(topSubcats, ctx);
 
+        case "getSubCatProducts":
+          const resp = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+            select: ["*"],
+            where: { feat: true },
+            populate: {
+              catagory: true,
+              products: {
+                populate: {
+                  images: true,
+                  varients: { populate: { colors: true } },
+                },
+              },
+            },
+          });
+
+          const limitedProducts = resp.map(subcat => ({
+            ...subcat,
+            products: subcat.products?.slice(0, 4) || [],
+          }));
+
+          return await this.sanitizeOutput(limitedProducts, ctx);
+
+        case "getAllSubcat":
+          const res = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+            select: ["*"],
+            populate: ["catagory", "img"],
+          });
+
+          return await this.sanitizeOutput(res, ctx);
+
+        case "getTopSubcats":
+          const orders = await strapi.db.query("api::order.order").findMany({
+            where: { status: "processed" },
+            select: ["cart"],
+          });
+
+          const productSalesMap = {};
+          orders.forEach(order => {
+            const cart = order.cart || [];
+            cart.forEach(item => {
+              const id = item.id;
+              const quantity = item.qty || 1;
+              if (!productSalesMap[id]) productSalesMap[id] = 0;
+              productSalesMap[id] += quantity;
+            });
+          });
+
+          const productIds = Object.keys(productSalesMap).map(id => Number(id));
+          const products = await strapi.db.query("api::product.product").findMany({
+            where: { id: { $in: productIds } },
+            populate: ["subcatagory"],
+          });
+
+          const productsWithSales = products.map(product => ({
+            ...product,
+            totalSales: productSalesMap[product.id] || 0,
+          }));
+
+          const subcategorySalesMap = {};
+          productsWithSales.forEach(product => {
+            const subcategory = product.subcatagory;
+            if (!subcategory) return;
+            const subcatId = subcategory.id;
+            if (!subcategorySalesMap[subcatId]) {
+              subcategorySalesMap[subcatId] = { subcategory, totalSales: 0, products: [] };
+            }
+            subcategorySalesMap[subcatId].totalSales += product.totalSales;
+            subcategorySalesMap[subcatId].products.push(product);
+          });
+
+          return Object.values(subcategorySalesMap)
+            .sort((a, b) => b.totalSales - a.totalSales)
+            .slice(0, 5);
+
+        default:
+          return "Not a valid function name (: ";
       }
     } else {
-
       var url_parts = url.parse(ctx.request.url, true);
       var query = url_parts.query;
       switch (query.func) {
-            case "getRelatedsubcats":
-         // return query.sid;
-            const ressub = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+        case "getTopSecSubcats":
+          const topSubcats = await strapi.db.query("api::subcatagory.subcatagory").findMany({
+            select: ["*"],
+            where: { topsec: true, status: true },
+            populate: ["img", "catagory", "products"],
+          });
+          return await this.sanitizeOutput(topSubcats, ctx);
 
-              select: ["*"],
-
-              where:{
-
-                catagory:{
-                  id:query.cid
-                },
-
-                },
-
-
-                filters:{
-                  $not:{
-                    id:query.sid
-                  }
-                },
-                populate: ["products","products.varients","products.varients.colors","products.subcatagory","products.seller"],
-            });
-
-            const sanitizedEntitysub = await this.sanitizeOutput(ressub, ctx);
-            return sanitizedEntitysub;
-
-            break;
-
-
-            case "getSubCatProducts":
-              const resp = await strapi.db.query("api::subcatagory.subcatagory").findMany({
-                select: ["*"],
-                where: {
-                  feat: true,
-                },
-                populate: {
-                  catagory: true,
-                  products: {
-                    populate: {
-                      images: true,
-                      varients: {
-                        populate: {
-                          colors: true,
-                        },
-                      },
-                    },
-                  },
-                },
-              });
-
-              const limitedProducts = resp.map(subcat => ({
-                ...subcat,
-                products: subcat.products?.slice(0, 4) || [],
-              }));
-
-              const sanitizedEntityp = await this.sanitizeOutput(limitedProducts, ctx);
-              return sanitizedEntityp;
-
-break;
-
-case "getAllSubcat":
-  const res = await strapi.db.query("api::subcatagory.subcatagory").findMany({
-    select: ["*"],
-    populate: ["catagory", "img"], // populate image
-  });
-
-                     const sanitizedEntity = await this.sanitizeOutput(res, ctx);
-
-                 //   console.dir(res);
-                     return sanitizedEntity;
-
-                     break;
-
-
-                     case "getTopSubcats":
-  const orders = await strapi.db.query("api::order.order").findMany({
-    where: { status: "processed" },
-    select: ["cart"],
-  });
-
-  const productSalesMap = {};
-
-  orders.forEach((order) => {
-    const cart = order.cart || [];
-    cart.forEach((item) => {
-      const id = item.id;
-      const quantity = item.qty || 1;
-      if (!productSalesMap[id]) productSalesMap[id] = 0;
-      productSalesMap[id] += quantity;
-    });
-  });
-
-  const productIds = Object.keys(productSalesMap).map((id) => Number(id));
-
-  const products = await strapi.db.query("api::product.product").findMany({
-    where: { id: { $in: productIds } },
-    populate: ["subcatagory",
-      //  "varients", "varients.colors"
-      ],
-  });
-
-  const productsWithSales = products.map((product) => {
-    const totalSales = productSalesMap[product.id] || 0;
-    return { ...product, totalSales };
-  });
-
-  const subcategorySalesMap = {};
-
-  productsWithSales.forEach((product) => {
-    const subcategory = product.subcatagory;
-    if (!subcategory) return;
-
-    const subcatId = subcategory.id;
-
-    if (!subcategorySalesMap[subcatId]) {
-      subcategorySalesMap[subcatId] = {
-        subcategory: subcategory,
-        totalSales: 0,
-        products: [],
-      };
-    }
-
-    subcategorySalesMap[subcatId].totalSales += product.totalSales;
-    subcategorySalesMap[subcatId].products.push(product);
-  });
-
-  const sortedSubcategories = Object.values(subcategorySalesMap)
-    .sort((a, b) => b.totalSales - a.totalSales)
-    .slice(0, 5);
-// console.log(sortedSubcategories.length);
-  return sortedSubcategories;
-            break;
-
-
-            default:
-
-            return "Not a valid function name (: ";
-
-            break;
+        default:
+          return "Not a valid function name (: ";
       }
     }
   },
 
-
-
-
-
   async update(ctx) {
     const { id } = ctx.params;
-
-    console.log("=== EditSubCat Request ===");
-    console.log("Params:", ctx.params);
-    console.log("Body:", ctx.request.body);
-    console.log("Files:", ctx.request.files);
 
     if (!ctx.request?.header?.authorization) {
       ctx.status = 403;
@@ -292,13 +219,9 @@ case "getAllSubcat":
     }
 
     const udata = await strapi.plugins["users-permissions"].services.jwt.getToken(ctx);
-    console.log("User data from JWT:", udata);
-
     const user = await strapi.entityService.findOne("plugin::users-permissions.user", udata.id);
-    console.log("User record:", user);
 
-    const utype = user.type;
-    if (utype !== 1) {
+    if (user.type !== 1) {
       ctx.status = 403;
       return { error: "Unauthorized" };
     }
@@ -307,25 +230,16 @@ case "getAllSubcat":
     let updateData = { name_ar, name_en, catagory, updatedAt: new Date() };
 
     if (ctx.request.files?.img) {
-      console.log("Uploading new image...");
       const files = Array.isArray(ctx.request.files.img) ? ctx.request.files.img : [ctx.request.files.img];
-      console.log("Files array:", files);
-
       try {
         const uploadedFiles = await strapi.plugin('upload').service('upload').upload({
           data: {},
           files,
         });
-
-        console.log("Uploaded files:", uploadedFiles);
-        if (uploadedFiles.length > 0) {
-          updateData.img = uploadedFiles[0].id;
-        }
+        if (uploadedFiles.length > 0) updateData.img = uploadedFiles[0].id;
       } catch (err) {
         console.error("Image upload failed:", err);
       }
-    } else {
-      console.log("No new image provided, keeping existing image.");
     }
 
     const updated = await strapi.entityService.update(
@@ -334,10 +248,34 @@ case "getAllSubcat":
       { data: updateData }
     );
 
-    console.log("Updated subcategory:", updated);
     return this.sanitizeOutput(updated, ctx);
   },
 
+  // ✅ New method to toggle topsec
+  async toggleTopsec(ctx) {
+    const { id } = ctx.params;
+    const { status } = ctx.request.body;
 
+    if (!ctx.request?.header?.authorization) {
+      ctx.status = 403;
+      return { error: "Unauthorized access" };
+    }
+
+    const udata = await strapi.plugins["users-permissions"].services.jwt.getToken(ctx);
+    const user = await strapi.entityService.findOne("plugin::users-permissions.user", udata.id);
+
+    if (user.type !== 1) {
+      ctx.status = 403;
+      return { error: "Unauthorized" };
+    }
+
+    const updated = await strapi.entityService.update(
+      "api::subcatagory.subcatagory",
+      id,
+      { data: { topsec: !!status, updatedAt: new Date() } }
+    );
+
+    return this.sanitizeOutput(updated, ctx);
+  },
 
 }));
